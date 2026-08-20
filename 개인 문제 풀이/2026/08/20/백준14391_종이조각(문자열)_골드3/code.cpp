f920bae9f91a099264db6e7d8a27a6a15d1f04fb
Eum Taewook <92018822+utsi09@users.noreply.github.com>
2026-08-20 22:24:48 +0900

    Update code.cpp

--- 개인 문제 풀이/2026/08/20/백준14391_종이조각(문자열)_골드3/code.cpp
+++ 개인 문제 풀이/2026/08/20/백준14391_종이조각(문자열)_골드3/code.cpp
@@ -1,43 +1,62 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int n,m;
-string paper[5];
-string paper2[5];
-int best = 0;
+int adj[5][5];
 
-void solve(const string p[5], int c, int r){
-    int cnt = 0;
-    for(int j=0; j<c; j++){
-        int this_num = 0;
-        for(int i=0; i<r; i++){
-            this_num = (this_num*10) + (p[i][j] - '0');
-            
-        }
-        cnt += this_num;
-        //cout << cnt << '\n';
-    }
-    best = max(best, cnt);
-    //cout << "best : " << best << '\n';
-    return;
-}
+int cnt = 0;
 
 int main(){
     cin >> n >> m;
-    for(int i=0; i<n; i++){
-        cin >> paper[i];
-        
-    }
-  
-    for(int j=0; j<m; j++){
-        for(int i=0; i<n; i++){
-            char c = paper[i][j];
-            paper2[j] += c;
-        } 
-    }
-    
-    solve(paper,m,n);
-    solve(paper2,n,m);
 
-    cout << best;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%1d", &adj[i][j]);
+        }
+    }
+
+
+    for(int s=0; s<(1 << 16)-1; s++){
+        
+        //가로 검사
+        int col = 0;
+        for(int i=0; i<n; i++){
+            
+            int cur = 0; //한 행에 쌓인 값
+            for(int j=0; j<m; j++){
+
+                if(s & 1 << (i * m + j)){ //격자의 특정 부분이 현재 무작위값 s 에서 0 즉 가로면 
+                    cur = cur*10 + adj[i][j];
+                }
+                else{
+                    col += cur;
+                    cur = 0;
+                }
+            }
+            col += cur;
+        }
+
+
+        //세로 검사
+        int row = 0;
+        for(int j=0; j<m; j++){
+           
+            int cur = 0; //한 행에 쌓인 값
+            for(int i=0; i<n; i++){
+
+                if(!(s & 1 << (i * m + j))){ //격자의 특정 부분이 현재 무작위값 s 에서 0 즉 가로면 
+                    cur = cur*10 + adj[i][j];
+                }
+                else{
+                    row += cur;
+                    cur = 0;
+                }
+            }
+            row += cur;
+        }
+        //cout << "this turn cnt is " << row + col << '\n';
+        cnt = max(cnt, row + col);
+    }
+    cout << cnt;
+
     return 0;
 }
